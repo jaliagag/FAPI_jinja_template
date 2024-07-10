e31a5abddb91794de28144a2e1468767c1acb558
jaliagag <jmfaliaga@gmail.com>
2024-07-10 10:28:22 -0300

    update

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi import Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from routers import products, timer, usuarios
+from routers.usuarios import get_users
 
 templates = Jinja2Templates(directory="templates/")
 
@@ -11,23 +12,28 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 # routers
-app.include_router(products.router)
+# app.include_router(products.router)
 app.include_router(timer.router)
 app.include_router(usuarios.router)
 
-usuarios = [{"name": "jose", "info": "some shit parks"},
-            {"name": "fede", "info": "some fede shit"},
-            {"name": "paula", "info": "some paula shit"}
-            ]
+# usuarios = [{"name": "jose", "info": "some shit parks"},
+#             {"name": "fede", "info": "some fede shit"},
+#             {"name": "paula", "info": "some paula shit"}
+#             ]
 
 @app.get("/")
 async def index(request: Request):
-    return templates.TemplateResponse("task/index.html", context={"request": request, "usuarios": usuarios})
+    try:
+        usuarios = await get_users()
+        print("usuarios: ", usuarios)
+        return templates.TemplateResponse("task/index.html", context={"request": request, "usuarios": usuarios})
+    except Exception as e:
+        return templates.TemplateResponse("task/index.html", context={"request": request})
 
 
-@app.get("/app")
-async def appme(request: Request):
-    return templates.TemplateResponse("pro/index.html", context={"request": request})
+# @app.get("/app")
+# async def appme(request: Request):
+#     return templates.TemplateResponse("pro/index.html", context={"request": request})
 
 # API endpoints
 
--- routers/usuarios.py
+++ routers/usuarios.py
@@ -1,18 +1,34 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status
+from fastapi.encoders import jsonable_encoder
+from pydantic import BaseModel
 
 router = APIRouter(prefix="/usuarios", 
                    tags=["usuarios"], # para la documentacion
                    responses={ 404: { "message": "not found" } }
                    )
 
+class Usuario(BaseModel):
+    name: str | None = None
+    info: str | None = None
+    # price: float | None = None
+    # tax: float = 10.5
+    # tags: list[str] = []
 
-usuarios = [{"name": "jose2", "info": "some shit parks"},
-            {"name": "fede2", "info": "some fede shit"},
-            {"name": "paula2", "info": "some paula shit"}
+usuarios = [{"name": "jose2", "info": "some ASDFASDFshit parks"},
+            {"name": "fede2", "info": "some fede shitASDFADSF"},
+            {"name": "paula2", "info": "some paula shit"},
+            {"name": "fede2", "info": "some fede shitASDFADSF"},
+            {"name": "asdf", "info": "some shit"}
             ]
 
 
 @router.get("/")
 async def get_users():
-    return {"users": "users", "value": usuarios}
+    return usuarios
+    # return {"users": "users", "value": usuarios2}
 
+@router.post("/", status_code=201, response_model=Usuario)
+async def update_users(user: Usuario):
+    update_user_encoded = jsonable_encoder(user)
+    usuarios.append(update_user_encoded)
+    return update_user_encoded
